14a98c4dca988195217b6612ada1c94e8f0af272
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-03 17:53:10 -0400

    Set up Spring Boot project with MongoDB Atlas

---
+++ src/main/java/ca/mcgillcssa/cssabackend/CssaBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgillcssa.cssabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssaBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CssaBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ca/mcgillcssa/cssabackend/CssaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgillcssa.cssabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
