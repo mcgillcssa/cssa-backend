12a68ae25d15bad6948473fe5abdd13343aead5a
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-04 02:57:46 -0400

    Remove unused imports

--- src/main/java/ca/mcgillcssa/cssabackend/config/MongoConfig.java
+++ src/main/java/ca/mcgillcssa/cssabackend/config/MongoConfig.java
@@ -12,9 +12,7 @@ import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class MongoConfig {
