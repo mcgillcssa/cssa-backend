ed17cb713f234c0dbeb2e30af95e692b84a44536
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-04 02:54:35 -0400

    Add start up log

--- src/main/java/ca/mcgillcssa/cssabackend/config/MongoConfig.java
+++ src/main/java/ca/mcgillcssa/cssabackend/config/MongoConfig.java
@@ -2,6 +2,7 @@ package ca.mcgillcssa.cssabackend.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoSocketException;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
@@ -11,7 +12,9 @@ import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class MongoConfig {
@@ -24,14 +27,21 @@ public class MongoConfig {
 
   @Bean
   public MongoClient mongoClient() {
-    MongoClientSettings settings = MongoClientSettings.builder()
-        .applyConnectionString(new ConnectionString(uri))
-        .serverApi(ServerApi.builder()
-            .version(ServerApiVersion.V1)
-            .build())
-        .build();
+    try {
+      MongoClientSettings settings = MongoClientSettings.builder()
+          .applyConnectionString(new ConnectionString(uri))
+          .serverApi(ServerApi.builder()
+              .version(ServerApiVersion.V1)
+              .build())
+          .build();
 
-    return MongoClients.create(settings);
+      MongoClient mongoClient = MongoClients.create(settings);
+      System.out.println("Connected to MongoDB!");
+      return mongoClient;
+    } catch (MongoSocketException e) {
+      System.err.println("Error connecting to MongoDB: " + e.getMessage());
+      return null;
+    }
   }
 
   @Bean
@@ -43,5 +53,4 @@ public class MongoConfig {
   public MongoTemplate mongoTemplate() {
     return new MongoTemplate(mongoClient(), database);
   }
-
 }
