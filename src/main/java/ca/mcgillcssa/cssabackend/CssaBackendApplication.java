12b26525115c8b6490b73bc073f6cd20ef3c446b
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-04 02:54:35 -0400

    Add start up log

--- src/main/java/ca/mcgillcssa/cssabackend/CssaBackendApplication.java
+++ src/main/java/ca/mcgillcssa/cssabackend/CssaBackendApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CssaBackendApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CssaBackendApplication.class, args);
+    try {
+      SpringApplication app = new SpringApplication(CssaBackendApplication.class);
+      app.run(args);
+      System.out.println("Backend now running on port 8080!");
+    } catch (Exception e) {
+      System.err.println("An error occurred while running the application:");
+      e.printStackTrace(System.err);
+    }
   }
-
 }
