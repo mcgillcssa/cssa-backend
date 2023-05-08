890a2d74c348956406641d7cd5b247971c9fa4bd
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-05-08 10:29:33 -0400

    [refac] Remove unused imports

--- src/main/java/ca/mcgillcssa/cssabackend/model/MembershipBenefit.java
+++ src/main/java/ca/mcgillcssa/cssabackend/model/MembershipBenefit.java
@@ -1,8 +1,5 @@
 package ca.mcgillcssa.cssabackend.model;
 
-
-import java.time.LocalDate;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,31 +7,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
-
 @Document(collection = "benefits")
 @Data
 @AllArgsConstructor
 @ToString
 public class MembershipBenefit {
-    @Id
-    private String merchantName;
-    private String merchantBackgroundImageUrl;
-    private String merchantDiscount;
-    private String merchantLogoUrl;
-    private MerchantType merchantType;
+  @Id
+  private String merchantName;
+  private String merchantBackgroundImageUrl;
+  private String merchantDiscount;
+  private String merchantLogoUrl;
+  private MerchantType merchantType;
 
-    public enum MerchantType {
-        RESTAURANT("主食"),
-        SWEETS("甜品饮品"),
-        SHOPPING("购物"),
-        BEAUTY("美甲美发"),
-        OTHER("其他");
-        private final String value;
-        MerchantType(String value) {
-            this.value = value;
-        }
-        public String getValue() {
-            return value;
-        }
+  public enum MerchantType {
+    RESTAURANT("主食"),
+    SWEETS("甜品饮品"),
+    SHOPPING("购物"),
+    BEAUTY("美甲美发"),
+    OTHER("其他");
+
+    private final String value;
+
+    MerchantType(String value) {
+      this.value = value;
     }
+
+    public String getValue() {
+      return value;
+    }
+  }
 }
--- src/main/java/ca/mcgillcssa/cssabackend/model/Sponsor.java
+++ src/main/java/ca/mcgillcssa/cssabackend/model/Sponsor.java
@@ -1,7 +1,5 @@
 package ca.mcgillcssa.cssabackend.model;
 
-import java.time.LocalDate;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,31 +13,31 @@ import lombok.ToString;
 @ToString
 
 public class Sponsor {
-    @Id
-    private String sponsorName;
-    private CoopDuration coopDuration;
-    private String sponsorImageUrl;
-    private String sponsorWebsiteUrl;
-    private SponsorClass sponsorClass;
+  @Id
+  private String sponsorName;
+  private CoopDuration coopDuration;
+  private String sponsorImageUrl;
+  private String sponsorWebsiteUrl;
+  private SponsorClass sponsorClass;
 
-    public enum CoopDuration {
-        季度合作,
-        全年合作;
+  public enum CoopDuration {
+    季度合作,
+    全年合作;
 
-        @Override
-        public String toString() {
-            return name();
-        }
+    @Override
+    public String toString() {
+      return name();
     }
+  }
 
-    public enum SponsorClass {
-        PLATINUM,
-        GOLD,
-        SILVER;
+  public enum SponsorClass {
+    PLATINUM,
+    GOLD,
+    SILVER;
 
-        @Override
-        public String toString() {
-            return name();
-        }
+    @Override
+    public String toString() {
+      return name();
     }
+  }
 }
