e2fdfb7076afb04002b88a25d30224f897485ff6
StevenTian0 <zhenyuan.tian@mail.mcgill.ca>
2023-05-07 23:44:23 -0400

    [feat] Sponsor Model

---
+++ src/main/java/ca/mcgillcssa/cssabackend/model/Sponsor.java
@@ -0,0 +1,45 @@
+package ca.mcgillcssa.cssabackend.model;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Document(collection = "sponsors")
+@Data
+@AllArgsConstructor
+@ToString
+
+public class Sponsor {
+    @Id
+    private String sponsorName;
+    private CoopDuration coopDuration;
+    private String imageUrl;
+    private String websiteUrl;
+    private SponsorClass sponsorClass;
+
+    public enum CoopDuration {
+        季度合作,
+        全年合作;
+
+        @Override
+        public String toString() {
+            return name();
+        }
+    }
+
+    public enum SponsorClass {
+        PLATINUM,
+        GOLD,
+        SILVER;
+
+        @Override
+        public String toString() {
+            return name();
+        }
+    }
+}
