b64d214eb98ce33ee2502b92917fde3893d2c214
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-09-24 18:43:48 -0400

    [fix] Hotfix properties and test bugs

--- src/test/java/ca/mcgillcssa/cssabackend/CssaBackendApplicationTests.java
+++ src/test/java/ca/mcgillcssa/cssabackend/CssaBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CssaBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  // @Test
+  // void contextLoads() {
+  // }
 
 }
