3f24d88ed85e23e58c4d9d2fe09b5b4268a93e79
Karencheenn <karen.chenlai@mail.mcgill.ca>
2024-09-11 15:15:14 +0800

    [docs] Update merchant types

--- src/main/java/ca/mcgillcssa/cssabackend/util/MerchantTypeChecker.java
+++ src/main/java/ca/mcgillcssa/cssabackend/util/MerchantTypeChecker.java
@@ -10,7 +10,7 @@ public class MerchantTypeChecker {
   }
 
   public static boolean isValidMerchantType(String type) {
-    List<String> validTypes = Arrays.asList("主食，餐厅", "甜品，饮品", "生活，娱乐", "购物", "其他");
+    List<String> validTypes = Arrays.asList("主食，餐厅", "甜品，饮品", "生活，娱乐", "购物", "其他", "餐厅 Restaurants", "甜点饮品 Desserts & Drinks", "生活娱乐 Life & Entertainment", "购物 Shopping");
     return validTypes.contains(type);
   }
 }
